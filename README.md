b2fea00b3b3acfa9fee48ffe6cba36f106a2dd21
mu_xin <h@ph233.cn>
2021-07-21 17:30:32 +0800

    创建模块

---
+++ README.md
@@ -0,0 +1,38 @@
+# Magisk Module Pixel Enabler China For VoTEL
+
+## Description
+
+This module will modify mbn_sw.txt to enable VoLTE for Chinese Operators , Theoretical support for all Pixel models and Android versions .
+
+## Author 
+
+[MuXin](https://github.com/CHN-MuXin "Author")
+
+[h@ph233.cn](h@ph233.cn "email")
+
+## Links
+
+[GitHub](https://github.com/CHN-MuXin/MagiskModuleEnableChinaForVoTELtoPIxel "Source code ")
+
+
+
+---
+
+
+# Magisk Pixel启用中国VoTEL 模块
+
+
+
+## 描述
+
+该模块通过修改 mbn_sw.txt 文件实现开启中国 VoLTE支持 以让Pixel能使用中国电信的 VoLTE通话 ，理论上支持Pixel所有型号以及安卓版本。
+
+## 作者
+
+[MuXin](https://github.com/CHN-MuXin "Author")
+
+[h@ph233.cn](h@ph233.cn "email")
+
+## 链接
+
+[GitHub](https://github.com/CHN-MuXin/MagiskModuleEnableChinaForVoTELtoPIxel "源代码")
---
+++ customize.sh
@@ -0,0 +1,43 @@
+#
+# This file is part of Pixel Enabler China  For VoTEL.
+#
+# Pixel Enabler China  For VoTEL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Copyright (C) 2021 Pixel Enabler China  For VoTEL Contributors
+#
+
+# shellcheck disable=SC2034
+SKIPUNZIP=1
+
+VERSION=$(grep_prop version "${TMPDIR}/module.prop")
+ui_print "- Pixel Enabler China  For VoTEL version ${VERSION}"
+ui_print "- Pixel 启用中国 VoTEL 模块 版本号 ${VERSION}"
+# Install
+ui_print "- Start Install"
+ui_print "- 开始安装"
+
+DEVICE=`getprop ro.product.system.brand`
+
+if [ $DEVICE != "google" ]; then
+  ui_print "=>不支持的品牌，如果要强制安装可在github下载Releases版"
+  abort " => Device '"$DEVICE"' is not supported yet，If you want to force the installation, please go to github to download the release version "
+fi
+
+# Extract Files
+ui_print "- Extracting module files"
+ui_print "- 导出模块文件"
+unzip -o "$ZIPFILE" -d "$MODPATH"
+
+# Edit mbn_sw.txt
+SY=/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/
+MS=$MODPATH/system/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/mbn_sw.txt
+find "$SY" -name '*.mbn' 2>&1  | grep -oi "mcfg_sw/.*/Commercial/.*\.mbn" > "$MS"
+
+# Set perm
+set_perm_recursive "$MODPATH" 0 0 0755 0644
+ui_print "- The installation is complete !"
+ui_print "- 安装完成!"
+ui_print "- Welcome to Pixel Enabler China  For VoTEL version Module!"
+ui_print "-  欢迎使用 Pixel 启用中国 VoTEL 模块!"
